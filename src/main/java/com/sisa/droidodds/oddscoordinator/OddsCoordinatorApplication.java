db5d88de140a0b2d87c609919367dd71c2921386
Laszlo Sisa <v-lsisa@expedia.com>
2018-12-14 14:38:09 +0100

    skeleton

---
+++ src/main/java/com/sisa/droidodds/oddscoordinator/OddsCoordinatorApplication.java
@@ -0,0 +1,14 @@
+package com.sisa.droidodds.oddscoordinator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OddsCoordinatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OddsCoordinatorApplication.class, args);
+	}
+
+}
+
